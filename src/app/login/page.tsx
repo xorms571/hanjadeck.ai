d27e746e80000a48d590e56bb7e3e850cd061f28
xorms571 <xorms6865@naver.com>
2025-11-11 07:07:51 +0900

    style: update global styles for dark mode and add button cursor style
    feat: create a new login page with image and buttons for user authentication

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+export default function LoginPage () {
+    return (
+        <div className="flex flex-col gap-6 justify-center items-center font-bold mt-24">
+            <h3 className="text-[64px]">Hello there!</h3>
+            <Image src="/login.svg" alt="Login Image" width={360} height={360} className="mb-7" />
+            <button className="bg-[#5A1BD1CC]/80 w-full max-w-[480px] h-[58px] text-white rounded-[180px]" >Get started</button>
+            <span>or</span>
+            <button className="bg-[#F8F8F8] w-full max-w-[480px] h-[58px] flex justify-center items-center gap-4 rounded-[180px]" ><Image src='/google.svg' width={24} height={24} alt="Google Sign In Icon" />Log in with Google</button>
+        </div>
+    )
+}
