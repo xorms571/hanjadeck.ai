a64b8991b295e663485893135adfad28089d9070
xorms571 <xorms6865@naver.com>
2025-11-11 12:13:33 +0900

    style: enhance Dashboard layout with max width and responsive text sizes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,8 +5,8 @@ import OverallProgress from "@/app/components/OverallProgress";
 
 export default function Dashboard() {
     return (
-        <div className="min-h-dvh">
-            <h1 className="mb-12">Welcome back, Hannah!</h1>
+        <div className="min-h-dvh max-w-[630px] lg:max-w-max mx-auto">
+            <h1 className="mb-12 text-[48px]! lg:text-[64px]!">Welcome back, Hannah!</h1>
             <div className="grid grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-6">
                 <div>
                     <Image src='/dashboard.svg' width={630} height={393} alt="dashboard image" />
