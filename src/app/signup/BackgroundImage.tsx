a4e8c024cb1db0edc9f13eae93ebe12e6cba3f5c
xorms571 <xorms6865@naver.com>
2025-11-15 16:34:53 +0900

    feat: Refactor BackgroundImage component for improved image handling and transitions; update globals.css to hide background images on smaller screens

--- src/app/signup/BackgroundImage.tsx
+++ src/app/signup/BackgroundImage.tsx
@@ -1,34 +1,12 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function BackgroundImage({ currentStep }: { currentStep: number }) {
-  const [image, setImage] = useState("/login.svg");
-  const [show, setShow] = useState(false)
-
-  useEffect(() => {
-    setShow(false);
-
-    const timer = setTimeout(() => {
-      if (currentStep === 0) {
-        setImage("/login.svg");
-      } else if (currentStep === 1) {
-        setImage("/set-a-goal.svg");
-      } else if (currentStep === 2) {
-        setImage("/proficiency.svg");
-      } else {
-        setImage("/complete.svg");
-      }
-      setShow(true);
-    }, 100); // Small delay to allow re-render
-
-    return () => clearTimeout(timer);
-  }, [currentStep]);
-
   return (
-    show ?
-      <Image className="absolute fade-up0 -z-10 top-1/2 -translate-y-1/2 left-9/12" src={image} width={840} height={840} alt={`${image} background image at signup page`} />
-      : null
+    <>
+      <Image className={`${currentStep === 0 ? "opcity-100 translate-x-0" : "opacity-0"} transition-all duration-800 -translate-x-96 backgoundImage absolute -z-10 top-1/2 -translate-y-1/2 left-9/12 login`} src="/login.svg" width={840} height={840} alt="login background image at signup page" />
+      <Image className={`${currentStep === 1 ? "opcity-100 translate-x-0" : "opacity-0"} transition-all duration-800 -translate-x-96 backgoundImage absolute -z-10 top-1/2 -translate-y-1/2 left-9/12 set-a-goal`} src="/set-a-goal.svg" width={840} height={840} alt="set-a-goal background image at signup page" />
+      <Image className={`${currentStep === 2 ? "opcity-100 translate-x-0" : "opacity-0"} transition-all duration-800 -translate-x-96 backgoundImage absolute -z-10 top-1/2 -translate-y-1/2 left-9/12 proficiency`} src="/proficiency.svg" width={840} height={840} alt="proficiency background image at signup page" />
+      <Image className={`${currentStep === 3 ? "opcity-100 translate-x-0" : "opacity-0"} transition-all duration-800 -translate-x-96 backgoundImage absolute -z-10 top-1/2 -translate-y-1/2 left-9/12 complete`} src="/complete.svg" width={840} height={840} alt="complete background image at signup page" />
+    </>
   );
 }
