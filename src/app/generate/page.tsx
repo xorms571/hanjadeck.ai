4a9919afa73ba0edb5c8d1ddadcfeb634524b3ca
xorms571 <xorms6865@naver.com>
2026-03-10 22:22:45 +0900

    Update exampleCard selection logic to use card ID instead of Korean property

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default async function GeneratePage() {
     const cards = await getCards(undefined);
-    const exampleCard = cards.find(card => card.korean === '예시') || cards[0];
+    const exampleCard = cards.find(card => card.id === 'cmmkn2bqq0000jy04ird89jjs') || cards[0];
 
     return (
         <main className="w-full py-12">
